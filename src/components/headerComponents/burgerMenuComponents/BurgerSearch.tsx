263078fe4780608c5b77800bd27a0b31f42b89b1
Ilya Chigrinov <chig-ilya2010@yandex.ru>
2026-02-25 22:28:14 +0300

    refactor: заменить position:absolute на flexbox в Search и BurgerSearch
      - SearchStyled/BurgerSearchStyled стали flex-контейнерами с
      overflow:hidden и focus-within
      - убраны position/top/transform/padding-right с магическими числами у
      кнопок и разделителя
      - исправлен селектор :last-child → & > :last-child в BurgerMenu
      - убран setIsActive из onClickClear в BurgerSearch (меню не должно
      сворачиваться при очистке)

--- src/components/headerComponents/burgerMenuComponents/BurgerSearch.tsx
+++ src/components/headerComponents/burgerMenuComponents/BurgerSearch.tsx
@@ -8,82 +8,90 @@ import styled from 'styled-components'
 const BurgerSearchStyled = styled.div`
     margin-left: auto;
     margin-right: auto;
-    position: relative;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 25px;
+    border-radius: 25px;
+    background: rgba(255, 255, 255, 0.1);
+    box-shadow: 0 0 0 1px rgba(255, 255, 255, 0.15);
+    overflow: hidden;
+    transition: box-shadow 0.3s;
+
+    &:focus-within,
+    &:hover {
+        box-shadow: 0 0 12px rgba(255, 102, 0, 0.5), 0 0 0 1px #ff6600;
+    }
 `
 const BurgerSearchInputStyled = styled.input`
-    width: 250px;
-    border-radius: 25px;
-    padding-left: 30px;
-    height: 25px;
+    flex: 1;
+    height: 100%;
+    padding-left: 12px;
     font-size: 16px;
-    line-height: 25px;
-    transition: 0.3s;
-    color: #1c1f22;
-
-    &:focus,
-    &:hover {
-        box-shadow: 0 0 10px #ff6600;
-    }
+    color: #e8e8e8;
+    background: transparent;
+    border: none;
+    outline: none;
 
     &::placeholder {
-        color: #cccccc;
+        color: #888888;
     }
 `
 const BurgerSearchButtonStyled = styled.button`
-    height: 25px;
+    flex-shrink: 0;
+    height: 100%;
     width: 30px;
-    position: absolute;
-    top: 50%;
-    left: 0;
-    transform: translateY(-50%);
-    background-color: inherit;
-    border-top-left-radius: 25px;
-    border-bottom-left-radius: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: transparent;
     transition: 0.3s;
-    stroke: #1c1f22;
+    stroke: #c8c8c8;
 
     & svg {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
+        transition: 0.3s;
     }
 
     &:hover {
-        background-color: #b84900;
+        background-color: #ff6600;
+        stroke: #ffffff;
     }
 
     & a {
-        display: block;
+        display: flex;
+        align-items: center;
+        justify-content: center;
         width: 100%;
         height: 100%;
     }
 `
+const BurgerDivider = styled.span`
+    flex-shrink: 0;
+    width: 1px;
+    height: 14px;
+    background: rgba(255, 255, 255, 0.2);
+`
 const BurgerClearButtonStyled = styled.button`
-    padding-right: 5px;
-    height: 25px;
-    width: 30px;
-    position: absolute;
-    top: 50%;
-    right: 0;
-    transform: translateY(-50%);
-    background-color: inherit;
-    border-top-right-radius: 25px;
-    border-bottom-right-radius: 25px;
+    flex-shrink: 0;
+    height: 24px;
+    width: 26px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: transparent;
     transition: 0.3s;
-    stroke: #1c1f22;
+    stroke: #c8c8c8;
 
     & svg {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
+        transition: 0.3s;
     }
 
     &:hover {
-        background-color: #b84900;
+        background-color: #ff6600;
+        stroke: #ffffff;
     }
 `
+
 type BurgerSearchProps = {
     setIsActive: React.Dispatch<React.SetStateAction<boolean>>
 }
@@ -109,7 +117,6 @@ const BurgerSearch = ({ setIsActive }: BurgerSearchProps) => {
         dispatch(paginationSlice.actions.animeListSetCurrentPage(0))
         setValue('')
         dispatch(animeSlice.actions.animeClearSearch())
-        setIsActive((prev) => !prev)
     }
 
     const onKeyPressEnter = (e: any) => {
@@ -121,20 +128,23 @@ const BurgerSearch = ({ setIsActive }: BurgerSearchProps) => {
     return (
         <BurgerSearchStyled className="search-wrapper" onClick={(e: React.MouseEvent<HTMLElement>) => e.stopPropagation()}>
             <BurgerSearchInputStyled value={value} onChange={onChange} onKeyDown={onKeyPressEnter} type="text" placeholder={'Search anime...'} className={'search-input'} />
+            {value && (
+                <BurgerClearButtonStyled onClick={onClickClear}>
+                    <svg width="14" height="14" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <line x1="2.64645" y1="17.6464" x2="17.6464" y2="2.64645" />
+                        <line x1="2.35355" y1="2.64645" x2="17.3536" y2="17.6464" />
+                    </svg>
+                </BurgerClearButtonStyled>
+            )}
+            <BurgerDivider />
             <BurgerSearchButtonStyled onClick={onClickSearch}>
                 <Link to={'/anime'}>
-                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="7" cy="8" r="6" />
                         <line x1="11.3254" y1="11.6204" x2="18.3254" y2="17.6204" />
                     </svg>
                 </Link>
             </BurgerSearchButtonStyled>
-            <BurgerClearButtonStyled onClick={onClickClear}>
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <line x1="2.64645" y1="17.6464" x2="17.6464" y2="2.64645" />
-                    <line x1="2.35355" y1="2.64645" x2="17.3536" y2="17.6464" />
-                </svg>
-            </BurgerClearButtonStyled>
         </BurgerSearchStyled>
     )
 }
